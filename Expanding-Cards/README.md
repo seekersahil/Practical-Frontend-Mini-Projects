2f1da2248ccfe1d0709ae590474c301253902c24
Sahil Gupta <seekersahil@gmail.com>
2023-01-05 14:10:50 +0530

    Added Expanding Cards

---
+++ Expanding-Cards/README.md
@@ -0,0 +1,3 @@
+# Expanding Cards
+
+Live Demo: https://dev.seekersahil.com/projects/DOM/Expanding-Cards
---
+++ Expanding-Cards/app.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", (e) => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach((panel) => panel.classList.remove("active"));
+}
